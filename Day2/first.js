c2809825e6797a5c1b57cd02b970ffc0d5f82896
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-19 00:01:19 +0500

    Add files via upload

---
+++ Day2/first.js
@@ -0,0 +1,66 @@
+// // simple console what it do
+// console.log("ALI");
+
+// // string datatype
+// let a = "sheraz";
+// console.log(a);
+
+// // // number datatype
+// let age = 20;
+// console.log(age);
+
+// // // boolean  datatype
+// let is_graduatede = true;
+// console.log(is_graduatede);
+
+
+
+// // /// bigint is used for large number and in output the last digit is used at the end of integer value
+// // in big_int datatype at the end n is used
+
+// let hugenumber = BigInt("11232454357887782132");
+// console.log(hugenumber);
+// console.log(typeof hugenumber);
+
+
+
+// // // symbol is used to create unique value
+// let symbol = Symbol("Sheraz");
+// console.log(symbol);
+
+// // null datatype
+// let type = null;
+// console.log(type);
+
+// // undefined dataype
+// let c;
+// console.log(c);
+// console.log(typeof c);
+
+
+// // non primitive datatypes
+// // array
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+
+
+
+// // object
+
+// let obj = {
+//     Name: "sheraz",
+//     age: 20,
+//     is_graduated: false,
+//     university: "GCUF",
+//     country: "pakistan",
+
+
+// }
+// console.log(obj);
+
+
+// // function
+// function Hello() {
+//     console.log("HI we are learning js ");
+// }
+// Hello();
---
+++ Day2/readme.txt
@@ -0,0 +1,11 @@
+1. Naming Rules:
+Allowed Characters:
+Variable names can consist of letters (a-z, A-Z), digits (0-9), underscores (_), and dollar signs ($). 
+Starting Character:
+Variable names must begin with a letter, an underscore (_), or a dollar sign ($). They cannot start with a digit. 
+No Spaces:
+Variable names cannot contain spaces. Use camelCase (e.g., firstName) or underscores (e.g., first_name) for multi-word names.
+Case-Sensitive:
+JavaScript variable names are case-sensitive. Name and name are considered different variables.
+Reserved Keywords:
+You cannot use JavaScript's reserved keywords (e.g., function, if, const, let, var, return) as variable names.
