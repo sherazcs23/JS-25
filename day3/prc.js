3db89e14b6ee95d851ec917250ff1a3a5f1d039a
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-19 10:42:22 +0500

    Create prc.js

---
+++ day3/prc.js
@@ -0,0 +1,176 @@
+// date 26 06 2024  started ths topic
+// let nam2 = "sheraz";
+// console.log(nam2);
+
+// let age = 25;
+// console.log(age);
+// console.log(typeof age);
+
+// let isGraduated = false;
+// console.log(isGraduated);
+// console.log(typeof isGraduated);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// let aba = null;
+// console.log(aba);
+// console.log(typeof aba);
+// console.log(typeof null); // null is an object in JavaScript
+
+ /* const students = {
+   name: "Sheraz Hussain",
+   age  : 25,
+   cgpa : 3.5,
+   isGraduated : false,
+   _title : "BS Comuter SCIENCE",
+   _completion_year : 2026,
+   $month : "August"
+  }
+ console.log(students);
+ console.log(students["_title"]);
+ console.log(students.name)
+ students.name = students.name + " " + "Khalid";
+ console.log(students.name);
+ students.age = students.age + 1;
+ console.log(students.age);
+ console.log(typeof students["age"]);
+ console.log(typeof students["_title"]);
+ console.log(typeof students["cgpa"]);
+ console.log(typeof students.isGraduated);
+ students.cgpa = students.cgpa + 0.3;
+ console.log(students.cgpa);*/
+
+   //  26 june 2024 arithematic oprerators 
+
+//   let a =7;
+//   let b =2;
+//  let c=a+b;
+// console.log(c); //9
+
+
+// let d=a-b;
+// console.log(d); //5
+
+
+// let e=a+b;
+// console.log(e); //9
+
+
+// let f=a**b;
+// console.log(f); //49
+
+// //division
+// let g=a/b;
+// console.log(g); //3.5
+
+// //modulus
+// let h=a%b;
+// console.log(h);  //1
+
+
+//   // unaray operator incement and decrement 
+//   let i =7;
+//   i++;
+//   console.log(i);
+//   // console.log(i+1);
+  
+//   let z = 10;
+//  z--;
+//    console.log(z);
+// // console.log(z-1);
+
+//pre increment and post increment
+//pre incremeent first change then output
+// let j =5;
+// console.log(++j); //6 
+
+//  // post increment next line mein add kary ga
+//   let k = 4;
+//   console.log(k++); //4
+//   console.log(k);//5
+
+
+//   //pre decrement
+//   let i = 7;
+//   console.log(--i); //6
+
+//   //post decremnt 
+
+//   let y = 10;
+//   console.log(y--);//10
+//   console.log(y); //9
+
+/*Assignemnt operators*/
+// let a = 4;
+// let  b = 6;
+//  a **= 2;
+//  console.log("a =",a);
+// console.log( "a*b =", a*b);
+// a /= 2;
+// console.log("a =",a);
+// b %= 3;
+// console.log("b =",b);
+
+// console.log(a+=2);
+// console.log(b-=4);
+
+
+
+// comparsion operators;
+// returns boolena value
+// equal to 
+
+//  let a = 5;
+//  let b = 5;
+//  console.log(a == b); //true
+
+
+// let c = "5";
+//  let d = 5;
+//  console.log(c==d);//true
+
+// not equal to;
+
+//  let a = 4;
+//  let b = 6;
+//  console.log(a != b);
+
+
+ // eqault to and type;
+ //  strcitly check value an type of data stored
+
+//  let c = "5";
+//  let d = 5;
+//  console.log(c===d); //false
+
+//  let i = 5;
+//  let j = 5;
+//  console.log(i===j); // true 
+
+//  let c = 8;
+//  let d = 3;
+//   console.log(c!==d);
+
+ //  logical operators 
+
+  // logica And opeartor &&
+//   let a = 7;
+//   let b = 6;
+//   let c = a > b;
+//   let d = a != b;;
+//   console.log(c && d); //true
+
+// // logical or operator ||
+//   let e = 7;
+//   let f = 6;
+//   let h = e > f;
+//   let g = e == f;
+//   console.log(e > f || e == f); //true
+ 
+  //logical not ! operator;
+
+  // let age = 25;
+  // let Agenextyear = 26;
+  // console.log(!(age == Agenextyear)); // true
