798d4c1b33a9854754d66870c98b4177742df1cb
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-21 23:50:40 +0500

    practice
    this is practice my question

---
+++ Day6/practice.js
@@ -0,0 +1,42 @@
+// const evenoddsepractor = (arr) => {
+//     const result = {
+//         even: [],
+//         odd: []
+//     }
+//     for (let num in arr) {
+//         if (num % 2 === 0) {
+//             result.even.push(num)
+//         }
+//         else {
+//             result.odd.push(num)
+//         }
+//     }
+//     return result;
+// }
+// let newarray = [1, 2, 3, 4, 56, 7, 1000, 89, 10];
+// let doublearr = evenoddsepractor(newarray);
+// console.log(doublearr);
+
+
+// question 2
+
+// let brr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 545, 3, 3, 2, 2, 111, 3, 42, 5, 3, 2, 4, 5, 6, 7, 8, 89];
+// let ctr = [...new Set(brr)];
+// console.log(ctr);
+
+
+// let b = {
+//     name: "sheraz",
+//     class: "bscs",
+//     age: 24,
+//     univeristy: "gcuf"
+// }
+// console.log(b["age"]);
+// console.log(b.name);
+// // for (let keys in b) {
+// //     console.log(keys);
+
+// //     console.log(b[keys]);
+// // }
+// console.log(Object.keys(b));
+// console.log(Object.values(b));
