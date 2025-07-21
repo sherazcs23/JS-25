9639083c5dfb34da36728b6d764db1322fe95686
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-21 11:41:19 +0500

    Update test.js

--- Day5/test.js
+++ Day5/test.js
@@ -1,9 +1,9 @@
  let arr = [1,2,3,4,5,6,7,8,9];
-//     console.log(delete arr[5]);
-//     console.log(arr);
+     console.log(delete arr[5]);
+    console.log(arr);
 
-//   let arr = [1,2,3,45,5,6];
-//    console.log(3 in arr);
+   let arr = [1,2,3,45,5,6];
+    console.log(3 in arr);
 
 // let arr1 = ["faisal", "sheraz", "khalid", "ali"];
 //     console.log([1] in arr1);
