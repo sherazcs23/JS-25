7db25f33a21e22cf40b31632cbc5f91c23892134
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-21 11:44:47 +0500

    Update test.js

--- Day5/test.js
+++ Day5/test.js
@@ -1,140 +1,139 @@
- let arr = [1,2,3,4,5,6,7,8,9];
-     console.log(delete arr[5]);
+let arr = [1,2,3,4,5,6,7,8,9];
+    console.log(delete arr[5]);
     console.log(arr);
 
-   let arr = [1,2,3,45,5,6];
-    console.log(3 in arr);
+  let arr = [1,2,3,45,5,6];
+   console.log(3 in arr);
 
-// let arr1 = ["faisal", "sheraz", "khalid", "ali"];
-//     console.log([1] in arr1);
+let arr1 = ["faisal", "sheraz", "khalid", "ali"];
+    console.log([1] in arr1);
 
-    //  const students = {
-    // name: "Sheraz Hussain",
-    // age: 25,
-    // cgpa: 3.5,
-    // isGraduated: false,
-    // _title: "BS Computer SCIENCE",
-    // _completion_year: 2026,
-    // $month: "August"
+ const students = {
+name: "Sheraz Hussain",
+age: 25,
+cgpa: 3.5,
+isGraduated: false,
+_title: "BS Computer SCIENCE",
+_completion_year: 2026,
+$month: "August"
 
-    //   }
-    //   console.log("name" in students);
-    //   console.log("age" in students);
-
-
-      // date 27 06 2025
-/// for loop
+  }
+  console.log("name" in students);
+  console.log("age" in students);
 
 
-   
-  //  for( let a=1; a<=10;a++){
-  //   console.log("3 *", a,"=", 3*a);
-  //  } 
-
-
-// for (let i = 0; i < 10; i++) {
-//     console.log("Hello World");
-//   }
+date 27 06 2025
+/ for loop
 
 
 
-  // let multiply = 5;
-  // for(let i=1; i<6; i++) {
-  //     multiply = multiply *i;
-  // }
-  //  console.log(" multiply =", multiply);
-  //  console.log("loop has ended"); 
+ for( let a=1; a<=10;a++){
+  console.log("3 *", a,"=", 3*a);
+ } 
 
 
-  // infinite loop that never ends;
+for (let i = 0; i < 10; i++) {
+    console.log("Hello World");
+  }
 
 
-//  for (let i = 0; i < 10; i--) {
-//     console.log("Hello World");
-     
-      //  while loop
-    //  let i = 1;
-    //  while (i <= 10) {
-    //   console.log("Hello World");
-    //   i++;
-    //  } 
-    
 
-    //do while loop //
-    // let f = 0;
-    //  do {
-    // console.log(f);
-    // f++;
-
-    //  } while(f < 20);
+let multiply = 5;
+for(let i=1; i<6; i++) {
+    multiply = multiply *i;
+}
+ console.log(" multiply =", multiply);
+ console.log("loop has ended"); 
 
 
- // for-of loop  used in arrays and strings
-      //  let srt = "lahore is city of colleges";
-      //  let  size = 0;
-      //  for (let vl of  srt)  {
-
-      //    console.log("value is =", vl);
-      //   size++;
-      //  }
-
-      //  console.log("size of string is =", size);
-      //  console.log(srt.length); // length
+infinite loop that never ends;
 
 
-    //  let  arr =[1,6,2,3,4,5,6,7,7,8,66,45634,5,45,4,];
-    //    let size = 0;
-    //    console.log(arr);
-    //  for(let val of arr) {
-    //   console.log("value is =",val)
-    //   size++;
-    //  }
-    // console.log(size);
-    // console.log(arr.length);
-    // console.log(arr[0]);
- 
+ for (let i = 0; i < 10; i--) {
+    console.log("Hello World");
+
+ while loop
+ let i = 1;
+ while (i <= 10) {
+  console.log("Hello World");
+  i++;
+ } 
 
 
-   // for-in loop is used in objects and array
-      //  let size = 0;
-      //  let obj = {
-      //    name: "Sheraz Hussain",       
-      //    age: 25,
-      //    cgpa: 3.5,
-      //   isGraduated: false,
-      //    _title: "BS Computer SCIENCE",  
-      //  }
-      //  for(let key in obj) {
-      //   console.log("key =", key);
-      //   size++;
-      //    console.log("value =", obj[key]);
-      //  }
-      //  console.log(size);
-       
-      // practice question
+do while loop //
+let f = 0;
+ do {
+console.log(f);
+f++;
 
-    //  for(let i=0; i<=100;i++){
-    //  if(i%2===0){
-    //   //even nmbers prints
-    //  console.log("even number =",i)  
-    //  }
-    //  // odd nmber prints
-    //  else {
-    //   console.log("odd number=",i); 
-    //  }
-    //  }
-    
+ } while(f < 20);
 
-   // 2nd question  
 
-    //  let usernum = prompt("enter number");
-    //   let gamenum = 25;
+for-of loop  used in arrays and strings
+ let srt = "lahore is city of colleges";
+ let  size = 0;
+ for (let vl of  srt)  {
 
-    //   if(usernum == gamenum) {
-    //     console.log("you have won the geme number");
-    
-    //   }
-    //     else if(usernum != gamenum) {
-    //    console.log("you have eneter wrong number");
-    //  }
+   console.log("value is =", vl);
+  size++;
+ }
 
+ console.log("size of string is =", size);
+ console.log(srt.length); // length
+
+
+ let  arr =[1,6,2,3,4,5,6,7,7,8,66,45634,5,45,4,];
+   let size = 0;
+   console.log(arr);
+ for(let val of arr) {
+  console.log("value is =",val)
+  size++;
+ }
+console.log(size);
+console.log(arr.length);
+console.log(arr[0]);
+
+
+
+for-in loop is used in objects and array
+ let size = 0;
+ let obj = {
+   name: "Sheraz Hussain",       
+   age: 25,
+   cgpa: 3.5,
+  isGraduated: false,
+   _title: "BS Computer SCIENCE",  
+ }
+ for(let key in obj) {
+  console.log("key =", key);
+  size++;
+   console.log("value =", obj[key]);
+ }
+ console.log(size);
+
+practice question
+
+ for(let i=0; i<=100;i++){
+ if(i%2===0){
+  //even nmbers prints
+ console.log("even number =",i)  
+ }
+ // odd nmber prints
+ else {
+  console.log("odd number=",i); 
+ }
+ }
+
+
+2nd question  
+
+ let usernum = prompt("enter number");
+  let gamenum = 25;
+
+  if(usernum == gamenum) {
+    console.log("you have won the geme number");
+
+  }
+    else if(usernum != gamenum) {
+   console.log("you have eneter wrong number");
+ }
