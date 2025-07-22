3b8fca30c0ca89b6b677654db7b4d00eb442c7f4
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2025-07-22 17:01:43 +0500

    Add files via upload

---
+++ Day6/function.js
@@ -0,0 +1,43 @@
+// // (function () {
+// //     let userName = prompt("Enter your name:");
+// //     console.log("Your name is " + userName);
+// // })();
+
+
+// // annonymous function
+// let showName = function (name) {
+//     console.log("Your name is " + name);
+// }
+
+// let userName = prompt("Enter your name:");
+// showName(userName);
+
+// //  anonymous function
+// // (function (name) {
+// //     console.log("Welcome " + name);
+// // })("Shahzad");
+
+
+// // function hello() {
+// //     console.log("Helo World");
+// // }
+// // hello();
+
+
+// let b = function () {
+//     console.log("Helo World");
+// }
+// b();
+
+
+// let c = () => {
+//     console.log("Helo World");
+// }
+// c();
+
+
+
+// (function (z) {
+//     console.log(z);
+// })(4);
+
